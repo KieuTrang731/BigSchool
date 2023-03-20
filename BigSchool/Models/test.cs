4b451495aff4d19102af12db9b406dfb0589c1ef
KieuTrang731 <kieutrang2002vn@gmail.com>
2023-03-20 09:41:22 +0700

    update 1

---
+++ BigSchool/Models/test.cs
@@ -0,0 +1,14 @@
+﻿using BigSchool.Migrations;
+using Microsoft.Owin.Security.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class test
+    {
+        int a;
+    }
+}
